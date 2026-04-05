c2797d559ad628175031ed712e536060db9db3d1
Faqih001 <fakiiahmad001@gmail.com>
2026-04-05 16:29:46 +0300

    feat: remove custom User model and add migration to delete it

--- backend/connector/models.py
+++ backend/connector/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
-class User(AbstractUser):
-    pass
 
 class DatabaseConnection(models.Model):
     DB_TYPES = [
